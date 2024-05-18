f86025332eca2504402cb2575e42c8c563922ba4
Rikard <mohlin.rikard@gmail.com>
2024-05-18 11:39:04 +0200

    added about info text and contact information

--- apps/frontend-wte/src/app/pages/about-page/about-page.component.ts
+++ apps/frontend-wte/src/app/pages/about-page/about-page.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
+import { FooterComponent } from '../../components/footer.component'
+import { HeaderComponent } from '../../components/header/header.component'
 
 @Component({
   selector: 'app-about-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss'
 })
