37e912b5db15322ea00e81662d405151be12c50d
Rikard <mohlin.rikard@gmail.com>
2024-05-18 11:59:14 +0200

    added styling to the about page

--- apps/frontend-wte/src/app/pages/about-page/about-page.component.ts
+++ apps/frontend-wte/src/app/pages/about-page/about-page.component.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
-import { FooterComponent } from '../../components/footer.component'
 import { HeaderComponent } from '../../components/header/header.component'
 
 @Component({
   selector: 'app-about-page',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, FooterComponent],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss'
 })
