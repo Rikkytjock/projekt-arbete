bb80440ed29740c70803379ae91b25435980730c
Rikard <mohlin.rikard@gmail.com>
2024-05-18 11:50:27 +0200

    added routing option to be able to navigate to about page

--- apps/frontend-wte/src/app/app.routes.ts
+++ apps/frontend-wte/src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Route } from '@angular/router'
+import { AboutPageComponent } from './pages/about-page/about-page.component'
 import { HomePageComponent } from './pages/home-page/home-page.component'
 
-export const appRoutes: Route[] = [{ path: '', component: HomePageComponent }]
+export const appRoutes: Route[] = [
+  { path: '', component: AboutPageComponent },
+  { path: '', component: HomePageComponent }
+]
