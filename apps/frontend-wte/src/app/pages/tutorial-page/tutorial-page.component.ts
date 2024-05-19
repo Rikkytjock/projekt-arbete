14e31c4a754015bd0d649af9deec0134448fb152
Henrik Frank <frank.havsvinge@gmail.com>
2024-05-19 13:13:39 +0200

    Removed unused code and comment. Removed imports of header and footer in pages

--- apps/frontend-wte/src/app/pages/tutorial-page/tutorial-page.component.ts
+++ apps/frontend-wte/src/app/pages/tutorial-page/tutorial-page.component.ts
@@ -1,12 +1,10 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
-import { FooterComponent } from '../../components/footer.component'
-import { HeaderComponent } from '../../components/header/header.component'
 
 @Component({
   selector: 'app-tutorial-page',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, FooterComponent],
+  imports: [CommonModule],
   templateUrl: './tutorial-page.component.html',
   styleUrl: './tutorial-page.component.scss'
 })
