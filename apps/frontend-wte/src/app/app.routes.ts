a534b6b941241b12ef84693544c9ccdbd89de46b
Henrik Frank <frank.havsvinge@gmail.com>
2024-05-19 13:05:34 +0200

    Fixed routing

--- apps/frontend-wte/src/app/app.routes.ts
+++ apps/frontend-wte/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { AboutPageComponent } from './pages/about-page/about-page.component'
 import { HomePageComponent } from './pages/home-page/home-page.component'
 
 export const appRoutes: Route[] = [
-  { path: '', component: AboutPageComponent },
+  { path: 'about', component: AboutPageComponent },
   { path: '', component: HomePageComponent }
 ]
