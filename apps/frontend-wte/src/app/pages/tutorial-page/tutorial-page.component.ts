ee4b6b17f3454e7c6aae44e37374028c6b76413f
Rikard <mohlin.rikard@gmail.com>
2024-05-19 10:48:13 +0200

    added media query to header image so it fits phone and tablet

--- apps/frontend-wte/src/app/pages/tutorial-page/tutorial-page.component.ts
+++ apps/frontend-wte/src/app/pages/tutorial-page/tutorial-page.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
+import { FooterComponent } from '../../components/footer.component'
+import { HeaderComponent } from '../../components/header/header.component'
 
 @Component({
   selector: 'app-tutorial-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './tutorial-page.component.html',
   styleUrl: './tutorial-page.component.scss'
 })
