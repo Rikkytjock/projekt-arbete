3199243ff88cf48c19648d98fa8205a60cad6199
Rikard <mohlin.rikard@gmail.com>
2024-05-19 10:27:08 +0200

    added tutorial page

---
+++ apps/frontend-wte/src/app/pages/tutorial-page/tutorial-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'app-tutorial-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tutorial-page.component.html',
+  styleUrl: './tutorial-page.component.scss'
+})
+export class TutorialPageComponent {}
