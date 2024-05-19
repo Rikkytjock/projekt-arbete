a4e8f933512c1adb07b74907052b7c8a904e3ff4
Rikard <mohlin.rikard@gmail.com>
2024-05-19 14:00:44 +0200

    added routerlink in tutorial

--- apps/frontend-wte/src/app/pages/home-page/home-page.component.ts
+++ apps/frontend-wte/src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
