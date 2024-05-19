6185282b5ddb2bb3acea7b446533691bebcb30f0
Henrik Frank <frank.havsvinge@gmail.com>
2024-05-19 13:14:56 +0200

    Moved footer into folder

--- apps/frontend-wte/src/app/app.component.ts
+++ apps/frontend-wte/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core'
 import { RouterModule } from '@angular/router'
-import { FooterComponent } from './components/footer.component'
+import { FooterComponent } from './components/footer/footer.component'
 import { HeaderComponent } from './components/header/header.component'
 
 @Component({
