d6407a25ce967da5e30b5cb765ba4cc7d9f0fd79
Rikard <mohlin.rikard@gmail.com>
2024-05-20 12:34:15 +0200

    added shortDesciption in recipe data. Commented out some code at the end becouse it gave me error. Need to ask Hank about that

--- apps/frontend-wte/src/app/services/recipe/recipe.data.ts
+++ apps/frontend-wte/src/app/services/recipe/recipe.data.ts
@@ -2,6 +2,7 @@ export interface IRecipe {
   id: string
   name: string
   description: string
+  shortDescription: string
   img: string
   cookingTime: ICookingTime
   ingredients: IIngredient[]
@@ -40,6 +41,7 @@ export const recipeData: IRecipe[] = [
     name: 'Daal',
     description:
       'A tasty dish with lentils, tomato and spices. It goes very fast to cook. It is a refreshing dish and contains nutrious things for the belly.',
+    shortDescription: 'Delicous lentils with indian spices',
     img: 'assets/images/tablet-and-food.png',
     cookingTime: {
       hours: 0,
@@ -85,56 +87,56 @@ export const recipeData: IRecipe[] = [
         description: 'Throw mustard seeds in pan until they pop.'
       }
     ]
-  },
-  {
-    id: '6649f6f389ea6c8a536f950c',
-    name: 'Blabla dish',
-    description:
-      'Lorem sakogkao fa lsmalsmfl amdfma slkm flkasmklfmas kfmalks mfkasmflkasnfkansk fmaslkfm alsmfklasmnfkl nakfnas klfm klasmf',
-    img: 'assets/images/tablet-and-food.png',
-    cookingTime: {
-      hours: 1,
-      minutes: 30
-    },
-    ingredients: [
-      {
-        name: 'Red lentils',
-        quantity: {
-          amount: 500,
-          unit: Unit.Grams
-        }
-      },
-      {
-        name: 'Cucumber',
-        quantity: {
-          amount: 1,
-          unit: Unit.Pieces
-        }
-      },
-      {
-        name: 'Weird spices',
-        quantity: {
-          amount: 100,
-          unit: Unit.Grams
-        }
-      }
-    ],
-    steps: [
-      {
-        stepNumber: 1,
-        name: 'Throw some spices in the trash',
-        description: 'Throw around 50 grams of weird spices in trash. This is a ritual.'
-      },
-      {
-        stepNumber: 2,
-        name: 'Fry lentils dry',
-        description: 'You want to burn them a little.'
-      },
-      {
-        stepNumber: 3,
-        name: 'Add cucumber',
-        description: 'Throw in the cucumber and fry it for 1 hour.'
-      }
-    ]
   }
+  //   {
+  //     id: '6649f6f389ea6c8a536f950c',
+  //     name: 'Blabla dish',
+  //     description:
+  //       'Lorem sakogkao fa lsmalsmfl amdfma slkm flkasmklfmas kfmalks mfkasmflkasnfkansk fmaslkfm alsmfklasmnfkl nakfnas klfm klasmf',
+  //     img: 'assets/images/tablet-and-food.png',
+  //     cookingTime: {
+  //       hours: 1,
+  //       minutes: 30
+  //     },
+  //     ingredients: [
+  //       {
+  //         name: 'Red lentils',
+  //         quantity: {
+  //           amount: 500,
+  //           unit: Unit.Grams
+  //         }
+  //       },
+  //       {
+  //         name: 'Cucumber',
+  //         quantity: {
+  //           amount: 1,
+  //           unit: Unit.Pieces
+  //         }
+  //       },
+  //       {
+  //         name: 'Weird spices',
+  //         quantity: {
+  //           amount: 100,
+  //           unit: Unit.Grams
+  //         }
+  //       }
+  //     ],
+  //     steps: [
+  //       {
+  //         stepNumber: 1,
+  //         name: 'Throw some spices in the trash',
+  //         description: 'Throw around 50 grams of weird spices in trash. This is a ritual.'
+  //       },
+  //       {
+  //         stepNumber: 2,
+  //         name: 'Fry lentils dry',
+  //         description: 'You want to burn them a little.'
+  //       },
+  //       {
+  //         stepNumber: 3,
+  //         name: 'Add cucumber',
+  //         description: 'Throw in the cucumber and fry it for 1 hour.'
+  //       }
+  //     ]
+  //   }
 ]
