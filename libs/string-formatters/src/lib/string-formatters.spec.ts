6c78f7d1d27a177d097fdd0bc2eb6eea394b8155
Rikard <mohlin.rikard@gmail.com>
2024-05-21 14:09:16 +0200

    generated libs in nx

---
+++ libs/string-formatters/src/lib/string-formatters.spec.ts
@@ -0,0 +1,7 @@
+import { stringFormatters } from './string-formatters'
+
+describe('stringFormatters', () => {
+  it('should work', () => {
+    expect(stringFormatters()).toEqual('string-formatters')
+  })
+})
