232fa769b831d1747f9f314aef89bbfc73b9c679
Rikard <mohlin.rikard@gmail.com>
2024-05-23 09:54:35 +0200

    added 8 recipe with images and fixed styling

--- apps/frontend-wte/src/app/services/recipe/recipe.data.ts
+++ apps/frontend-wte/src/app/services/recipe/recipe.data.ts
@@ -87,12 +87,384 @@ export const recipeData: IRecipe[] = [
     ]
   },
 
+  // RECEPT 2
+
   {
     id: '6649f6f389ea6c8a536f950c',
-    name: 'Blabla dish',
-    description:
-      'Lorem sakogkao fa lsmalsmfl amdfma slkm flkasmklfmas kfmalks mfkasmflkasnfkansk fmaslkfm alsmfklasmnfkl nakfnas klfm klasmf',
-    img: 'assets/images/tablet-and-food.png',
+    name: 'Beetroot Pasta',
+    description: 'Tasty pasta with beetroot and coconut milk',
+    img: 'assets/images/beetroot-pasta.jpg',
+    cookingTime: {
+      hours: 1,
+      minutes: 25
+    },
+    ingredients: [
+      {
+        name: 'Red lentils',
+        quantity: {
+          amount: 500,
+          unit: Unit.Grams
+        }
+      },
+      {
+        name: 'Cucumber',
+        quantity: {
+          amount: 1,
+          unit: Unit.Pieces
+        }
+      },
+      {
+        name: 'Weird spices',
+        quantity: {
+          amount: 100,
+          unit: Unit.Grams
+        }
+      }
+    ],
+    steps: [
+      {
+        stepNumber: 1,
+        name: 'Throw some spices in the trash',
+        description: 'Throw around 50 grams of weird spices in trash. This is a ritual.'
+      },
+      {
+        stepNumber: 2,
+        name: 'Fry lentils dry',
+        description: 'You want to burn them a little.'
+      },
+      {
+        stepNumber: 3,
+        name: 'Add cucumber',
+        description: 'Throw in the cucumber and fry it for 1 hour.'
+      }
+    ]
+  },
+
+  // RECEPT 3
+
+  {
+    id: '6649f6f389ea6c8a536f950c',
+    name: 'Chana Masala',
+    description: 'Chickpeas in spicy indian curry',
+    img: 'assets/images/chana-masala.jpg',
+    cookingTime: {
+      hours: 1,
+      minutes: 30
+    },
+    ingredients: [
+      {
+        name: 'Red lentils',
+        quantity: {
+          amount: 500,
+          unit: Unit.Grams
+        }
+      },
+      {
+        name: 'Cucumber',
+        quantity: {
+          amount: 1,
+          unit: Unit.Pieces
+        }
+      },
+      {
+        name: 'Weird spices',
+        quantity: {
+          amount: 100,
+          unit: Unit.Grams
+        }
+      }
+    ],
+    steps: [
+      {
+        stepNumber: 1,
+        name: 'Throw some spices in the trash',
+        description: 'Throw around 50 grams of weird spices in trash. This is a ritual.'
+      },
+      {
+        stepNumber: 2,
+        name: 'Fry lentils dry',
+        description: 'You want to burn them a little.'
+      },
+      {
+        stepNumber: 3,
+        name: 'Add cucumber',
+        description: 'Throw in the cucumber and fry it for 1 hour.'
+      }
+    ]
+  },
+
+  // RECEPT 4
+
+  {
+    id: '6649f6f389ea6c8a536f950c',
+    name: 'Swedish Bread',
+    description: 'Classical bread baked the swedish way',
+    img: 'assets/images/bread.jpg',
+    cookingTime: {
+      hours: 1,
+      minutes: 30
+    },
+    ingredients: [
+      {
+        name: 'Red lentils',
+        quantity: {
+          amount: 500,
+          unit: Unit.Grams
+        }
+      },
+      {
+        name: 'Cucumber',
+        quantity: {
+          amount: 1,
+          unit: Unit.Pieces
+        }
+      },
+      {
+        name: 'Weird spices',
+        quantity: {
+          amount: 100,
+          unit: Unit.Grams
+        }
+      }
+    ],
+    steps: [
+      {
+        stepNumber: 1,
+        name: 'Throw some spices in the trash',
+        description: 'Throw around 50 grams of weird spices in trash. This is a ritual.'
+      },
+      {
+        stepNumber: 2,
+        name: 'Fry lentils dry',
+        description: 'You want to burn them a little.'
+      },
+      {
+        stepNumber: 3,
+        name: 'Add cucumber',
+        description: 'Throw in the cucumber and fry it for 1 hour.'
+      }
+    ]
+  },
+
+  // RECEPT 5
+
+  {
+    id: '6649f6f389ea6c8a536f950c',
+    name: 'Dosa',
+    description: 'Indian style pancake made of lentils and rice',
+    img: 'assets/images/dosa.jpeg',
+    cookingTime: {
+      hours: 1,
+      minutes: 30
+    },
+    ingredients: [
+      {
+        name: 'Red lentils',
+        quantity: {
+          amount: 500,
+          unit: Unit.Grams
+        }
+      },
+      {
+        name: 'Cucumber',
+        quantity: {
+          amount: 1,
+          unit: Unit.Pieces
+        }
+      },
+      {
+        name: 'Weird spices',
+        quantity: {
+          amount: 100,
+          unit: Unit.Grams
+        }
+      }
+    ],
+    steps: [
+      {
+        stepNumber: 1,
+        name: 'Throw some spices in the trash',
+        description: 'Throw around 50 grams of weird spices in trash. This is a ritual.'
+      },
+      {
+        stepNumber: 2,
+        name: 'Fry lentils dry',
+        description: 'You want to burn them a little.'
+      },
+      {
+        stepNumber: 3,
+        name: 'Add cucumber',
+        description: 'Throw in the cucumber and fry it for 1 hour.'
+      }
+    ]
+  },
+
+  // RECEPT 6
+
+  {
+    id: '6649f6f389ea6c8a536f950c',
+    name: 'Chapati',
+    description: 'Flatbread made the indian way',
+    img: 'assets/images/chapati.webp',
+    cookingTime: {
+      hours: 1,
+      minutes: 30
+    },
+    ingredients: [
+      {
+        name: 'Red lentils',
+        quantity: {
+          amount: 500,
+          unit: Unit.Grams
+        }
+      },
+      {
+        name: 'Cucumber',
+        quantity: {
+          amount: 1,
+          unit: Unit.Pieces
+        }
+      },
+      {
+        name: 'Weird spices',
+        quantity: {
+          amount: 100,
+          unit: Unit.Grams
+        }
+      }
+    ],
+    steps: [
+      {
+        stepNumber: 1,
+        name: 'Throw some spices in the trash',
+        description: 'Throw around 50 grams of weird spices in trash. This is a ritual.'
+      },
+      {
+        stepNumber: 2,
+        name: 'Fry lentils dry',
+        description: 'You want to burn them a little.'
+      },
+      {
+        stepNumber: 3,
+        name: 'Add cucumber',
+        description: 'Throw in the cucumber and fry it for 1 hour.'
+      }
+    ]
+  },
+
+  // RECEPT 7
+
+  {
+    id: '6649f6f389ea6c8a536f950c',
+    name: 'Idly',
+    description: 'Steamed rice cakes',
+    img: 'assets/images/idly.jpg',
+    cookingTime: {
+      hours: 1,
+      minutes: 30
+    },
+    ingredients: [
+      {
+        name: 'Red lentils',
+        quantity: {
+          amount: 500,
+          unit: Unit.Grams
+        }
+      },
+      {
+        name: 'Cucumber',
+        quantity: {
+          amount: 1,
+          unit: Unit.Pieces
+        }
+      },
+      {
+        name: 'Weird spices',
+        quantity: {
+          amount: 100,
+          unit: Unit.Grams
+        }
+      }
+    ],
+    steps: [
+      {
+        stepNumber: 1,
+        name: 'Throw some spices in the trash',
+        description: 'Throw around 50 grams of weird spices in trash. This is a ritual.'
+      },
+      {
+        stepNumber: 2,
+        name: 'Fry lentils dry',
+        description: 'You want to burn them a little.'
+      },
+      {
+        stepNumber: 3,
+        name: 'Add cucumber',
+        description: 'Throw in the cucumber and fry it for 1 hour.'
+      }
+    ]
+  },
+
+  // RECEPT 8
+
+  {
+    id: '6649f6f389ea6c8a536f950c',
+    name: 'Pani Puri',
+    description: 'Spicy indian delicacy',
+    img: 'assets/images/pani-puri.jpeg',
+    cookingTime: {
+      hours: 1,
+      minutes: 30
+    },
+    ingredients: [
+      {
+        name: 'Red lentils',
+        quantity: {
+          amount: 500,
+          unit: Unit.Grams
+        }
+      },
+      {
+        name: 'Cucumber',
+        quantity: {
+          amount: 1,
+          unit: Unit.Pieces
+        }
+      },
+      {
+        name: 'Weird spices',
+        quantity: {
+          amount: 100,
+          unit: Unit.Grams
+        }
+      }
+    ],
+    steps: [
+      {
+        stepNumber: 1,
+        name: 'Throw some spices in the trash',
+        description: 'Throw around 50 grams of weird spices in trash. This is a ritual.'
+      },
+      {
+        stepNumber: 2,
+        name: 'Fry lentils dry',
+        description: 'You want to burn them a little.'
+      },
+      {
+        stepNumber: 3,
+        name: 'Add cucumber',
+        description: 'Throw in the cucumber and fry it for 1 hour.'
+      }
+    ]
+  },
+
+  // RECEPT 9
+
+  {
+    id: '6649f6f389ea6c8a536f950c',
+    name: 'Laddo',
+    description: 'Sweetness in a ball',
+    img: 'assets/images/laddo.jpeg',
     cookingTime: {
       hours: 1,
       minutes: 30
@@ -139,5 +511,3 @@ export const recipeData: IRecipe[] = [
     ]
   }
 ]
-
-// Gör funktion som gör en cutoff efter ett visst antal ord... Läs mer...
