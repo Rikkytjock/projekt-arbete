2edbe4d732d64c9b4f3d3cc651bc9ecab62063a4
Henrik Frank <frank.havsvinge@gmail.com>
2024-05-26 12:16:43 +0200

    Added string declaration as reutnr type for shortenString

--- libs/string-formatters/src/lib/string-formatters.ts
+++ libs/string-formatters/src/lib/string-formatters.ts
@@ -1,8 +1,4 @@
-// export function shortenString(str: string, length = 20): string {
-//   return '...'
-// }
-
-export function shortenString(str: string, length = 25) {
+export function shortenString(str: string, length = 25): string {
   if (str.length <= length) {
     return str
   } else {
