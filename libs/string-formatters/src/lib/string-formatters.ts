f3a3f7ce55e4a6b8bcc8cdf605945ebebe1ec750
Rikard <mohlin.rikard@gmail.com>
2024-05-26 11:43:04 +0200

    changed the parameter to length 25

--- libs/string-formatters/src/lib/string-formatters.ts
+++ libs/string-formatters/src/lib/string-formatters.ts
@@ -2,10 +2,10 @@
 //   return '...'
 // }
 
-export function shortenString(str: string, num: number) {
-  if (str.length <= 25) {
+export function shortenString(str: string, length = 25) {
+  if (str.length <= length) {
     return str
   } else {
-    return str.slice(0, num) + '...'
+    return str.slice(0, length) + '...'
   }
 }
