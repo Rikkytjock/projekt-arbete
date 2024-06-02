1a915a0fa23ff02ae8d251e7db7765f4cbfd3ab1
Rikard <mohlin.rikard@gmail.com>
2024-06-02 11:18:17 +0200

    added routing to the recpie list page to the recipe page

--- apps/frontend-wte/src/app/pages/recipe-list-page/recipe-page/recipe-page.component.ts
+++ apps/frontend-wte/src/app/pages/recipe-list-page/recipe-page/recipe-page.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'app-recipe-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './recipe-page.component.html',
   styleUrl: './recipe-page.component.scss'
 })
