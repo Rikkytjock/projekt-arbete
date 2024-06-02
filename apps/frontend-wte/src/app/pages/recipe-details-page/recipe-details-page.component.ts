5bf7c3daa0fa8b49f9fe260e45af69a83ffe486b
Rikard <mohlin.rikard@gmail.com>
2024-06-02 08:52:42 +0200

    added new page for detailed recipes

---
+++ apps/frontend-wte/src/app/pages/recipe-details-page/recipe-details-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'app-recipe-details-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './recipe-details-page.component.html',
+  styleUrl: './recipe-details-page.component.scss'
+})
+export class RecipeDetailsPageComponent {}
