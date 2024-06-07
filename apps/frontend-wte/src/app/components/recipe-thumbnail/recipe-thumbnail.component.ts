81f0bcdd23652ad5bcccbc810f25ec13707592c8
Rikard <mohlin.rikard@gmail.com>
2024-06-07 08:22:06 +0200

    moved functions to recipe thumbnail

--- apps/frontend-wte/src/app/components/recipe-thumbnail/recipe-thumbnail.component.ts
+++ apps/frontend-wte/src/app/components/recipe-thumbnail/recipe-thumbnail.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
+import { RouterLink } from '@angular/router'
+import { prettyHoursAndMinutes, shortenString } from '@inglorious/string-formatters'
+import { ICookingTime } from '../../services/recipe/recipe.data'
+import { RecipeService } from '../../services/recipe/recipe.service'
 
 @Component({
   selector: 'app-recipe-thumbnail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
+  providers: [RecipeService],
   templateUrl: './recipe-thumbnail.component.html',
   styleUrl: './recipe-thumbnail.component.scss'
 })
-export class RecipeThumbnailComponent {}
+export class RecipeThumbnailComponent {
+  showFullDescription = false
+
+  shortenDescription(description: string) {
+    return shortenString(description, 25)
+  }
+
+  detailedCookingTime(cookingTime: ICookingTime) {
+    return prettyHoursAndMinutes(cookingTime.hours, cookingTime.minutes)
+  }
+}
