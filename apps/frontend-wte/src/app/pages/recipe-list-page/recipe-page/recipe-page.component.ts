b52bd0dddf34b71a6fac384ca9abb0146ccf41e0
Rikard <mohlin.rikard@gmail.com>
2024-11-21 09:47:00 +0100

    importerade recipe data och skapade en this recipe find i ngoninit för att den skulle kunna stanna

--- apps/frontend-wte/src/app/pages/recipe-list-page/recipe-page/recipe-page.component.ts
+++ apps/frontend-wte/src/app/pages/recipe-list-page/recipe-page/recipe-page.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute, RouterLink } from '@angular/router'
 import { IRecipe } from '@inglorious/core-types'
 import { RecipeThumbnailComponent } from '../../../components/recipe-thumbnail/recipe-thumbnail.component'
+import { recipeData } from '../../../services/recipe/recipe.data'
 import { RecipeService } from '../../../services/recipe/recipe.service'
 
 @Component({
@@ -23,9 +24,12 @@ export class RecipePageComponent implements OnInit {
     private route: ActivatedRoute // Fattar inte riktigt vad denna gör ska kolla mer om det.
   ) {}
 
+  // Behöver använda recipeData för att den ska kunna importeras.
+
   async ngOnInit() {
     const id = this.route.snapshot.params['id']
     this.recipe = await this.recipeService.getById(id)
+    this.recipe = recipeData.find((recipe) => recipe.id === id)!
   }
 }
 
