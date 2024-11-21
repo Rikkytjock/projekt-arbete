857e62ce9223b6dc1b46cb435b784e26c76f5a47
Rikard <mohlin.rikard@gmail.com>
2024-11-21 09:36:24 +0100

    added html to recipe-page

--- apps/frontend-wte/src/app/pages/recipe-list-page/recipe-page/recipe-page.component.ts
+++ apps/frontend-wte/src/app/pages/recipe-list-page/recipe-page/recipe-page.component.ts
@@ -1,12 +1,32 @@
 import { CommonModule } from '@angular/common'
-import { Component } from '@angular/core'
-import { RouterLink } from '@angular/router'
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute, RouterLink } from '@angular/router'
+import { IRecipe } from '@inglorious/core-types'
+import { RecipeThumbnailComponent } from '../../../components/recipe-thumbnail/recipe-thumbnail.component'
+import { RecipeService } from '../../../services/recipe/recipe.service'
 
 @Component({
   selector: 'app-recipe-page',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, RecipeThumbnailComponent],
+  providers: [RecipeService],
   templateUrl: './recipe-page.component.html',
   styleUrl: './recipe-page.component.scss'
 })
-export class RecipePageComponent {}
+export class RecipePageComponent implements OnInit {
+  recipe!: IRecipe
+  // Ändrade till recipe! vilket gör att recipe endast kommer hålla ett recept och att det kommer bli assignat senare
+
+  // constructor injectar de services som jag behöver för komponenten.
+  constructor(
+    private recipeService: RecipeService, // Fetchar recipe data
+    private route: ActivatedRoute // Fattar inte riktigt vad denna gör ska kolla mer om det.
+  ) {}
+
+  async ngOnInit() {
+    const id = this.route.snapshot.params['id']
+    this.recipe = await this.recipeService.getById(id)
+  }
+}
+
+// Kanske ska ha ett try-catch block här för att hantera error om ID inte finns?
