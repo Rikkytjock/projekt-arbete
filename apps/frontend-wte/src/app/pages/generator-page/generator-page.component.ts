158868b40656cc2352a8164abe7a876b4c3b1a50
Rikard <mohlin.rikard@gmail.com>
2024-12-21 08:23:43 +0100

    created a generator page

---
+++ apps/frontend-wte/src/app/pages/generator-page/generator-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'app-generator-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './generator-page.component.html',
+  styleUrl: './generator-page.component.scss'
+})
+export class GeneratorPageComponent {}
