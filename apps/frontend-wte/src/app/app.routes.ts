21078c03c6acdf7301760171d978ab0d6f65a93b
Rikard <mohlin.rikard@gmail.com>
2024-12-21 08:51:48 +0100

    added vanilla html and css to create visuals of how it can look

--- apps/frontend-wte/src/app/app.routes.ts
+++ apps/frontend-wte/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Route } from '@angular/router'
 import { AboutPageComponent } from './pages/about-page/about-page.component'
+import { GeneratorPageComponent } from './pages/generator-page/generator-page.component'
 import { PrivacyPageComponent } from './pages/privacy-page/privacy-page.component'
 import { RecipeListPageComponent } from './pages/recipe-list-page/recipe-list-page.component'
 import { RecipePageComponent } from './pages/recipe-list-page/recipe-page/recipe-page.component'
@@ -11,5 +12,6 @@ export const appRoutes: Route[] = [
   { path: 'recipes', component: RecipeListPageComponent },
   { path: 'recipes/:recipeId', component: RecipePageComponent },
   { path: 'privacy', component: PrivacyPageComponent },
+  { path: 'generator', component: GeneratorPageComponent },
   { path: '', component: RecipeListPageComponent }
 ]
--- apps/frontend-wte/src/app/pages/generator-page/generator-page.component.ts
+++ apps/frontend-wte/src/app/pages/generator-page/generator-page.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'app-generator-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './generator-page.component.html',
   styleUrl: './generator-page.component.scss'
 })
